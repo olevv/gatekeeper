57ce61d0f92f02ecd3b8ebca801d456d61fbb2b5
vitaly <oleynikvit@gmail.com>
2020-07-18 21:37:53 +0300

    Added new integration tests

---
+++ tests/Application/Command/User/SignUp/SignUpHandlerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Application\Command\User\SignUp;
+
+final class SignUpHandlerTest
+{
+
+}
---
+++ tests/Application/Command/User/Unblock/UnblockHandlerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Application\Command\User\Unblock;
+
+final class UnblockHandlerTest
+{
+
+}
